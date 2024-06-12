1bc03147e515d78c6f4e38f60e9fcacabf6ff2c4
Samuel Wright <wsright987@gmail.com>
2024-06-12 01:30:29 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # thrillweb
+
+npm run dev
+npm run build
+firebase deploy --only hosting:thrillweb
---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="page-container">
+      <div className="info-container">
+        <div className='title info-text'>Thrillweb Digital</div>
+        <div className='info-text'>+1 609-472-2897</div>
+        <div className='info-text'>swrightdev@gmail.com</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
