3527a693de3d3f15ff8bfd2e1a1d7ada65add4e8
Samuel Wright <wsright987@gmail.com>
2024-07-23 06:43:59 -0400

    Hero style

--- next-client-typescript/app/page.tsx
+++ next-client-typescript/app/page.tsx
@@ -1,16 +1,19 @@
 "use client"; 
 
+import '../app/globals.css';
+import '../styles/home.css';
 import Footer from "./components/Footer";
 import HeroSection from "./components/home/HeroSection";
 import Navigation from "./components/Navigation";
-import '../app/globals.css';
-import '../styles/home.css';
+import HeroSectionSocialMedia from "./components/home/HeroSectionSocialMedia";
+
 
 export default function Home() {
   return (
     <main className="main">
       <Navigation />
       <HeroSection />
+      <HeroSectionSocialMedia />
       <Footer />
     </main>
   );
